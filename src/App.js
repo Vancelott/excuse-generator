cbd0fdc5979093c4cec0c9ec92c1cf310ca80d53
Vancelott <ivan.angelovv2@gmail.com>
2023-05-05 23:37:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import { useState, useEffect } from "react"
 
 function App() {
+  const [excuseParty, setExcuse] = useState("");
+
+  const fetchPartyExcuses = () => {
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/party").then((res) => {
+      setExcuse(res.data[0].excuse);
+    });
+  };
+  
+  // const fetchPartyExcuses2 = async () => {
+  //   try {
+  //     const response = await fetch("https://excuser-three.vercel.app/v1/excuse/party");
+  //     const data = await response.json();
+  //     console.log(data..excuse)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  
+
+  useEffect(() => {
+    fetchPartyExcuses();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate An Excuse</h1>
+      <button onClick={fetchPartyExcuses}>Party</button>
+      <button>Family</button>
+      <button>Office</button>
+      <p>{excuseParty}</p>
     </div>
   );
 }
