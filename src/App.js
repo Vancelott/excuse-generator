7c65d6dfbcc66aa2c1a2cd85cf4f90a1596c959d
Vancelott <ivan.angelovv2@gmail.com>
2023-05-06 09:46:00 +0300

    deleted a few useless lines

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import './App.css';
 import Axios from "axios";
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 function App() {
-  const [excuseParty, setExcuse] = useState("");
+  const [partyExcuse, setPartyExcuse] = useState("");
+  const [familyExcuse, setFamilyExcuse] = useState("");
+  const [officeExcuse, setOfficeExcuse] = useState("");
 
   const fetchPartyExcuses = () => {
     Axios.get("https://excuser-three.vercel.app/v1/excuse/party").then((res) => {
-      setExcuse(res.data[0].excuse);
+      setPartyExcuse(res.data[0].excuse);
     });
   };
-  
-  // const fetchPartyExcuses2 = async () => {
-  //   try {
-  //     const response = await fetch("https://excuser-three.vercel.app/v1/excuse/party");
-  //     const data = await response.json();
-  //     console.log(data..excuse)
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  
 
-  useEffect(() => {
-    fetchPartyExcuses();
-  }, []);
+  const fetchFamilyExcuses = () => {
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/family").then((res) => {
+      setFamilyExcuse(res.data[0].excuse);
+    });
+  };
+
+  const fetchOfficeExcuses = () => {
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/office/").then((res) => {
+      setOfficeExcuse(res.data[0].excuse);
+    });
+  };
 
   return (
     <div className="App">
       <h1>Generate An Excuse</h1>
       <button onClick={fetchPartyExcuses}>Party</button>
-      <button>Family</button>
-      <button>Office</button>
-      <p>{excuseParty}</p>
+      <button onClick={fetchFamilyExcuses}>Family</button>
+      <button onClick={fetchOfficeExcuses}>Office</button>
+      <p>Party excuse: {partyExcuse}</p>
+      <p>Family excuse: {familyExcuse}</p>
+      <p>Office excuse: {officeExcuse}</p>
     </div>
   );
 }
